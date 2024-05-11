039deb508e9c933f7bb1c4b1437b3cdccf2821e6
Marcel <me@m4rc3l.de>
2024-05-11 17:12:17 +0200

    routes: cleanup

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -50,7 +50,6 @@ pub(crate) fn route(content_paths: &ContentPaths) -> Router<FoundationState> {
     .route("/team/:lang", get(get_team))
     .nest_service("/team/assets", ServeDir::new(&content_paths.team))
     .route("/mailing_lists/:list", post(add_subscriber))
-    .route("/stats", get(get_traffic_stats))
     .route("/stats/traffic", get(get_traffic_stats))
     .route("/stats/as112", get(get_as112_stats))
     .route("/peers", get(get_peers_and_supporter))
